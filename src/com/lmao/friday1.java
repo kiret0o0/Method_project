e6957ccd5eb8d256376f27d6c3be8a6dbd58d535
kiret0o0 <kiril_18@outlook.com>
2019-07-13 01:31:20 +0300

    MySecondCommit

--- src/com/lmao/friday1.java
+++ src/com/lmao/friday1.java
@@ -1,15 +1,10 @@
 package com.lmao;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class friday1 {
 
     public static void main(String[] args){
 
-        InputStream in;
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
 
     }
 }
